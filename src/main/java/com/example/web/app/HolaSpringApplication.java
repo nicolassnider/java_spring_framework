bd2585af7b598ad48fc5a0e8943dd3f3cd9763e1
Nicolás Snider <nicolas.snider@soulit.io>
2020-12-11 21:13:32 -0300

    a

--- src/main/java/com/example/web/app/HolaSpringApplication.java
+++ src/main/java/com/example/web/app/HolaSpringApplication.java
@@ -2,17 +2,19 @@ package com.example.web.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
+
 
 public class HolaSpringApplication {
 
 	@GetMapping("/hola")
 	public String hola() {
-		return "hola mundo con spring";
+		return "index";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(HolaSpringApplication.class, args);
